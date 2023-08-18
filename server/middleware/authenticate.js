00c1ab478fe0d6c3342d494eb2e5bc3582bd1301
itsfarhankhan28 <famannat@gmail.com>
2023-08-18 16:31:22 +0530

    feat(authenticate): added the secret key

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
-const Schema = require('../model/signUpSchema') 
+const Schema = require('../model/signUpSchema')
+require('dotenv').config() 
 
 const authenticate = async(req,res,next)=>{
     try{
     //taking the cookie
     const token = req.cookies.token1
     //verify token
-    const verifytoken = jwt.verify(token , itisfarhankhanthefullstackwebdeveloper)
+    const verifytoken = jwt.verify(token , process.env.SECRET_KEY)
     //verify user
     const user = await Schema.findOne({_id:verifytoken._id})
 
