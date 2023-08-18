a3c9278fad91962163f78c872a048ebc9f105734
itsfarhankhan28 <famannat@gmail.com>
2023-08-18 18:32:37 +0530

    refactor(app): removed all comments

--- server/app.js
+++ server/app.js
@@ -1,32 +1,15 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
 const express = require('express')
 const cookie_parser = require('cookie-parser') 
 const app = express()
 const router = require('./api/route')
 
-//Middleware for telling express to accept the data in jason form(Has to be defined before routing otherwise it won't work)
 app.use(express.json())
 app.use(cookie_parser())
 
-//Middleware connection for router file
 app.use('/recipe',router)
 
-//require fileupload for cloudinary
-// const fileupload = require('express-fileupload')
-
-//env file contains the database link
-// dotenv.config({path:'./config.env'})
-
-//connection with file that contains connection with database
 require('./db/conn')
-
-//import routes
-// require('./router/route')
-
-// app.use(fileupload({
-//     useTempFiles:true
-// }))
 
 app.use(
     express.urlencoded({ extended: true })
